eb7c2a00d57c86ecd206448ce8fd46a15fa03c83
Thomas Schlatter <thomas.schlatter@shades.ch>
2019-11-07 17:15:00 +0100

    Remember me and Logout

--- src/main/java/ch/shades/demo/springit/security/SecurityConfiguration.java
+++ src/main/java/ch/shades/demo/springit/security/SecurityConfiguration.java
@@ -30,9 +30,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.antMatchers("/h2-console/**").permitAll()
 			.and()
 			.formLogin()
+				.loginPage("/login").permitAll()
+				.usernameParameter("email")
 			.and()
-			.csrf().disable()
-			.headers().frameOptions().disable();
+			.logout()
+			.and()
+			.rememberMe();
+//			.and()
+//			.csrf().disable()
+//			.headers().frameOptions().disable();
 		
 		//super.configure(http);
 	}
